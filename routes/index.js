8f06fb17427ef8bd0158df7b075d2977835bc6f1
JViniX <47263194+JViniX@users.noreply.github.com>
2023-05-29 20:52:43 -0400

    Week 03 - Express and EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+      { 
+      title: 'Home',
+      userName: 'Joanna'
+    });
+});
+
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', 
+    { 
+      title: 'About',
+      userName: 'John'
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('responds from root of the Users router');
+});
+
+router.get('/signin', function(req, res, next) {
+  res.send('responds from signin of the Users router');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.send('responds from signup of the Users router');
+});
+
+router.get('/signout', function(req, res, next) {
+  res.send('responds from signout of the Users router');
+});
+
+router.get('/errormessage', function(req, res, next) {
+  res.render('error', 
+    { 
+      message: 'Pay attention',
+      error: {
+        status: '503',
+        stack: 'lots of error messages'
+      }
+    });
 });
 
 module.exports = router;
